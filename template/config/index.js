ea34e5ee772bea6dd199aaaab26cb33a197faef3
Phat Phyrom <phyrom.phat@workevolve.com>
2017-11-20 13:08:11 +0700

    first commit

---
+++ template/config/index.js
@@ -0,0 +1,97 @@
+'use strict'
+// Template version: 1.2.3
+// see http://vuejs-templates.github.io/webpack for documentation.
+
+const fs = require('fs')
+const path = require('path')
+
+module.exports = {
+  // Various Dev Server settings
+  host: 'localhost', // can be overwritten by process.env.HOST
+  http: {
+    // default port where dev server listens for incoming traffic
+    // can be overwritten by process.env.HOST, if port is in use, a free one will be determined
+    port: process.env.HOST || 8080
+  },
+  https: {
+    port: 8443,
+    options: {
+      key: fs.readFileSync(path.resolve(process.cwd(), 'build/certs/localhost.key')),
+      cert: fs.readFileSync(path.resolve(process.cwd(), 'build/certs/localhost.crt')) /*,
+      requestCert: false,
+      rejectUnauthorized: false */
+    }
+  },
+  dev: {
+    env: require('./dev.env'),
+
+    // Paths
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+
+    // Use Eslint Loader?
+    // If true, your code will be linted during bundling and
+    // linting errors and warnings will be shown in the console.
+    useEslint: true,
+    // If true, eslint errors and warnings will also be shown in the error overlay
+    // in the browser.
+    showEslintErrorsInOverlay: false,
+
+    /**
+     * Source Maps
+     */
+
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: 'eval-source-map',
+
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: true,
+
+    // CSS Sourcemaps off by default because relative paths are "buggy"
+    // with this option, according to the CSS-Loader README
+    // (https://github.com/webpack/css-loader#sourcemaps)
+    // In our experience, they generally work as expected,
+    // just be aware of this issue when enabling this option.
+    cssSourceMap: false,
+  },
+
+  build: {
+    env: require('./prod.env'),
+
+    // Template for index.html
+    index: path.resolve(__dirname, '../dist/index.html'),
+
+    // Paths
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+
+    /**
+     * Source Maps
+     */
+
+    productionSourceMap: true,
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: '#source-map',
+
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  }
+}
